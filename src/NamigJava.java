407e6d4fc96c65a25e73f906b4ef9215a0c1a72d
Pedro Campodonico <pedrocampodonico12@gmail.com>
2023-05-11 20:20:59 -0500

    Creamos la clase NamigJava

---
+++ src/NamigJava.java
@@ -0,0 +1,23 @@
+public class NamigJava {
+
+    public static void main(String[] args) {
+        //Java diferencia las mayusculas de las minisculas
+        //y tambien de los simbolos
+        int celphone = 987765654;
+        int celPhone = 555555555;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency$ = "MXN";
+        String background_color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+    }
+
+}
